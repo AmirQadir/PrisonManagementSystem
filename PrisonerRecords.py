fd0802aff61c4bf99f837e6824ac731131a11880
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2019-04-07 22:36:06 +0500

    minor new files - testing better work practice

---
+++ PrisonerRecords.py
@@ -0,0 +1,47 @@
+import sqlite3
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
+from menu import Ui_Menu
+from sqlite3 import Error
+from table import Ui_Dialog
+import sys
+
+import PyQt5
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+ 
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+class PrisonerRecords:
+	def __init__(self):
+		self.conn = sqlite3.connect('prison.db')
+
+		self.diag = QtWidgets.QDialog()
+		self.table_ui = Ui_Dialog()
+		self.table_ui.setupUi(self.diag)
+		self.table_ui.pushButton.clicked.connect(self.viewRecords)
+		self.diag.exec_()
+
+	def viewRecords(self):
+		print("hello")
+		cur = self.conn.cursor()
+		cur.execute("SELECT * from Prisoner")
+
+		rows = cur.fetchall()
+
+		self.table_ui.tableWidget.setRowCount(len(rows)) #Number of records jitna
+
+		itr1 = 0
+		itr2 = 0
+		for record in rows:
+			itr2 = 0
+			for eachrecord in record:
+				print(eachrecord," ..")
+				self.table_ui.tableWidget.setItem(itr1,itr2,QTableWidgetItem(str(eachrecord)))
+				itr2 = itr2 + 1
+				# print("I am here", str(eachrecord))
+			itr1 = itr1 + 1
+
+			
---
+++ main.py
@@ -0,0 +1,61 @@
+import sqlite3
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
+from menu import Ui_Menu
+from sqlite3 import Error
+from table import Ui_Dialog
+import sys
+from PrisonerRecords import PrisonerRecords
+import PyQt5
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+ 
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+
+
+
+
+class Main:
+	
+	
+
+	def __init__(self):
+
+		#variables
+		self.diag = 0
+
+
+		print("constructor")
+		app = QtWidgets.QApplication(sys.argv)
+		menu = QtWidgets.QMainWindow()
+		menu_ui = Ui_Menu()
+		menu_ui.setupUi(menu)
+		menu.show()
+		menu_ui.pushButton.clicked.connect(self.prisonerRecords)
+		sys.exit(app.exec_())	
+	
+	def prisonerRecords(self):
+		# print("hello")
+		# self.diag = QtWidgets.QMainWindow()
+		# menu_ui = Ui_Menu()
+		# menu_ui.setupUi(self.diag)
+		# self.diag.show()
+		pop = PrisonerRecords()
+		
+
+
+
+
+		
+if __name__ == "__main__":
+	x = Main()
+
+
+
+
+
+
+
